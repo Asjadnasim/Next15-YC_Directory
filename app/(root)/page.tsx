930490d9674da0f91f5500a8b879724a81915e8b
Asjad Nasim <asjadnasim@gmail.com>
2024-11-03 16:00:43 +0530

    Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image';
+import SearchForm from '@/components/SearchForm';
 
-export default function Home() {
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ query?: string }>;
+}) {
+	const query = (await searchParams).query;
+
 	return (
 		<>
 			<section className='pink_container'>
@@ -11,6 +17,8 @@ export default function Home() {
 					Submit Ideas, Votes on Pitches and Get Noticed in Virtual
 					Competitions.
 				</p>
+
+				<SearchForm query={query} />
 			</section>
 		</>
 	);
