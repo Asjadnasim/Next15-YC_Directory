0bc6b2b445b44357b1fe665737658e24afb1f55d
Asjad Nasim <asjadnasim@gmail.com>
2024-11-04 15:55:10 +0530

    StartUp Card Design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/SearchForm';
+import StartUpCard from '@/components/StartUpCard';
 
 export default async function Home({
 	searchParams,
@@ -6,6 +7,26 @@ export default async function Home({
 	searchParams: Promise<{ query?: string }>;
 }) {
 	const query = (await searchParams).query;
+
+	const posts = [
+		{
+			createdAt: 'Yesterday',
+			title: 'We Robot',
+			category: 'Robotics',
+			image:
+				'https://media.istockphoto.com/id/1189903914/photo/robot-woman-sci-fi-android-female-artificial-intelligence-3d-render.webp?s=2048x2048&w=is&k=20&c=VfY4kehngTp_MHV6g_fYtvxyeuEUOs5Hcy2LG_gS3J4=',
+			description:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quae.',
+			_id: 1,
+			author: {
+				_id: 1,
+				name: 'John Doe',
+				image:
+					'https://media.istockphoto.com/id/1292931074/photo/young-caucasian-businessman-work-on-laptop-thinking.webp?s=2048x2048&w=is&k=20&c=iJ0G4etSXjxe_JtwwYAq0CIJtHMIlF5tIAJPpReI1m8=',
+			},
+			views: 1000,
+		},
+	];
 
 	return (
 		<>
@@ -19,6 +40,25 @@ export default async function Home({
 				</p>
 
 				<SearchForm query={query} />
+			</section>
+
+			<section className='section_container'>
+				<p className='text-30-semibold'>
+					{query
+						? `Search results for the 
+					"${query}"`
+						: 'All Startups'}
+				</p>
+
+				<ul className='mt-7 card_grid'>
+					{posts?.length > 0 ? (
+						posts.map((post: StartupCardType, index: number) => (
+							<StartUpCard key={post?._id} post={post} />
+						))
+					) : (
+						<p className='no-results'>No Start-ups found</p>
+					)}
+				</ul>
 			</section>
 		</>
 	);
